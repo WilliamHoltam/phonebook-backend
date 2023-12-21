ae01a416e6f5a998c65567990ce877865ca947e3
Will <22566733+WilliamHoltam@users.noreply.github.com>
2023-12-21 12:08:36 +0000

    part3-phonebook-validate

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
 
+app.use(cors())
+
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan('tiny', { skip: (req, res) => req.method === "POST" }))
@@ -16,27 +18,12 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
     skip: (req, res) => req.method !== "POST"
 }))
 
-app.use(cors())
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-
-    next(error)
-}
-
-app.use(errorHandler)
-
-app.get('/api/persons', (request, response, next) => {
+app.get('/api/persons', (request, response) => {
     Person.find({})
         .then(notes => response.json(notes))
-        .catch(error => next(error))
 })
 
-app.get('/info', (request, response, next) => {
+app.get('/info', (request, response) => {
     const now = new Date().toString();
     Person.countDocuments()
         .then(count => response.send(`
@@ -44,10 +31,10 @@ app.get('/info', (request, response, next) => {
                 <p>Phonebook has info for ${count} people</p>
                 <p>${now}</p>
             </div>`))
-        .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
+    console.log("Getting by ID")
     Person.findById(request.params.id)
         .then(person => {
             if (person) {
@@ -68,39 +55,48 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-
-    const person = {
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    const { name, number } = request.body
+    console.log("updating...")
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(updatedPerson => response.json(updatedPerson))
         .catch(error => next(error))
 })
 
+
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-
-    if (body.name === undefined) {
-        return response.status(400).json({ error: 'name missing' })
-    }
-
-    if (body.number === undefined) {
-        return response.status(400).json({ error: 'number missing' })
-    }
-
+    const { name, number } = request.body
+    console.log("posting...")
     const newPerson = new Person({
-        name: body.name,
-        number: body.number,
+        name: name,
+        number: number,
     })
-
+    console.log("it gets here")
     newPerson.save()
-        .then(newPerson => response.json(newPerson))
+        .then(savedPerson => response.json(savedPerson))
         .catch(error => next(error))
+    console.log("and here??")
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.log("This is running???")
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -15,8 +15,22 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: [true, 'Person name required'],
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: [true, 'Person phone number required'],
+        validate: {
+            validator: function (v) {
+                return (/(^$)|(\\d{2,3}-\\d+)/).test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`,
+        }
+    }
 })
 
 personSchema.set('toJSON', {
